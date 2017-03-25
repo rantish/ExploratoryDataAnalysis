e39a1139c44ef9696bd68902d2bdf2cdb495d28a
rantish <vikrantz887@gmail.com>
2017-03-25 12:48:28 +0530

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,6 @@
+
+#PLOT 1
+
+hist(inputdata$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
